013431632e3284874f29e7c2da9f743d56cf81f7
Ethan <ethanholohan1@gmail.com>
2025-04-29 10:41:09 +0100

    camera tracking added

---
+++ camera_3d.gd
@@ -0,0 +1,13 @@
+extends Camera3D
+
+@onready var Drone = get_parent().find_child("Drone").find_child("camtarget")
+@onready var cam = self
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float):
+	cam.global_position = lerp(cam.global_position, Drone.global_position, delta * 2)
+	cam.look_at(Drone.get_parent_node_3d().global_position, Vector3.UP)
+
+	pass
